4306d6c6473aaf8b743817323c1adb6488359d43
imran <mazaktak96@gmail.com>
2026-03-03 03:17:59 +0530

    this is v2

--- testing.py
+++ testing.py
@@ -1,1 +1,15 @@
-print("hello dosto")
+def print(
+    *values: object,
+    sep: str | None = " ",
+    end: str | None = "\n",
+    file: SupportsWrite[str] | None = None,
+    flush: Literal[False] = False
+) -> None: ...
+
+def print(
+    *values: object,
+    sep: str | None = " ",
+    end: str | None = "\n",
+    file: _SupportsWriteAndFlush[str] | None = None,
+    flush: bool ) ;
+    print("hello")
