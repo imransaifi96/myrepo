491ba4f260dc0a759289f3f7be81f40f629673fd
imransaifi96 <imransaifi110096@gmail.com>
2026-05-26 15:57:46 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,8 +1,1 @@
-def print(
-    *values: object,
-    sep: str | None = " ",
-    end: str | None = "\n",
-    file: SupportsWrite[str] | None = None,
-    flush: Literal[False] = False
-) -> None: ...
-
+hello akash this is version "V6"
