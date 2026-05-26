1e651ff80e988256f6f0a21cffac4336ace30f39
imransaifi96 <imransaifi110096@gmail.com>
2026-05-26 15:53:39 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,12 +6,3 @@ def print(
     flush: Literal[False] = False
 ) -> None: ...
 
-def print(
-    *values: object,
-    sep: str | None = " ",
-    end: str | None = "\n",
-    file: _SupportsWriteAndFlush[str] | None = None,
-    flush: bool ) ;
-    print("hello")
-    jhbkldcwljc
-    print("jhcjgc")
